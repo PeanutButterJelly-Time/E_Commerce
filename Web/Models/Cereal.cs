2549eca4d2571a89f19d1cedbf7a3ae8d43f2009
ArikMackenburg <Arikmackenburg@gmail.com>
2020-09-28 15:52:55 -0500

    View displaying

---
+++ Web/Models/Cereal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class Cereal : Product
+    {
+        public override string Name { get; set; }
+        public override string Manufacturer { get; set; }
+        public Type Type { get; set; }
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Fat { get; set; }
+        public double Sodium { get; set; }
+        public double Fiber { get; set; }
+        public double Carbo { get; set; }
+        public double Sugars { get; set; }
+        public double Potassium { get; set; }
+        public int Vitamins { get; set; }
+        public int Shelf { get; set; }
+        public double Weight { get; set; }
+        public double Cups { get; set; }
+        public override double Rating { get; set; }
+    }
+    public enum Type
+    {
+        Hot,
+        Cold,
+    }
+}
---
+++ Web/Services/ProductRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Services
+{
+    public class ProductRepo : IProduct
+    {
+        private readonly List<Cereal> Cereals = new List<Cereal>
+        {
+            new Cereal
+            {
+                Name = "Something",
+                Manufacturer = "Quakers Bitch Ass",
+                Rating = 0.2,
+            },
+            new Cereal
+            {
+                Name = "Something Else",
+                Manufacturer = "General Mills",
+                Rating = 1.2,
+            },
+        };
+        public IEnumerable<Cereal> GetCereals()
+        {
+            return Cereals;
+        }
+    }
+}
