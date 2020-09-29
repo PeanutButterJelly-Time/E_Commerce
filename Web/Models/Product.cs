2935918eafca9cad1e72dbf8aca8856483a60874
Keith Dahlby <keith@newbo.co>
2020-09-29 10:39:21 -0500

    abstract auto-properties aren't useful

--- Web/Models/Product.cs
+++ Web/Models/Product.cs
@@ -7,9 +7,10 @@ namespace Web.Models
 {
     public abstract class Product
     {
-        public abstract string Name { get; set; }
-        public abstract string Manufacturer { get; set; }
-        public abstract double Rating { get; set; }
-        
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public double Rating { get; set; }
     }
 }
