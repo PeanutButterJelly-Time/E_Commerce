a9c509b11b86dc27576ef29e31c52c05f124181e
ChristianLievrouw <celievrouw1@gmail.com>
2020-09-29 16:34:44 -0500

    registration page made

--- Web/Pages/Account/Register.cshtml.cs
+++ Web/Pages/Account/Register.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,36 @@ namespace Web.Pages
     {
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                return LocalRedirect("~/");
+            }
+            return Page();
+        }
+
+        [BindProperty]
+        public RegisterInput Input { get; set; }
+
+        public class RegisterInput
+        {
+            [Required]
+            [EmailAddress]
+
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare(nameof(Password))]
+            public string ConfirmPassword { get; set; }
         }
     }
 }
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -61,6 +61,7 @@ namespace Web
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
