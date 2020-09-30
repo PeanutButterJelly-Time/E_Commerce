14a98189384f046fff3ddfd7a8c1fb383cde2d1e
Keith Dahlby <keith@newbo.co>
2020-09-30 11:05:23 -0500

    Show all products and link to Cereal Details

--- Web/Pages/Product/Products.cshtml.cs
+++ Web/Pages/Product/Products.cshtml.cs
@@ -1,29 +1,24 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Web.Data;
-using Web.Models;
 
 namespace Web.Pages.Product
 {
     public class ProductsModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+
         public ProductsModel(ApplicationDbContext context)
         {
             _context = context;
         }
-        public List<Shoe> List = new List<Shoe>();
+
+        public List<Models.Product> List = new List<Models.Product>();
+
         public void OnGet()
         {
-            foreach (var product in _context.Products)
-            {
-                if(product.Discriminator == "Shoe")
-                    List.Add(new Shoe(){ Id = product.Id, Name = product.Name, Manufacturer = product.Manufacturer});
-            }
+            List = _context.Products.ToList();
         }
     }
 }
