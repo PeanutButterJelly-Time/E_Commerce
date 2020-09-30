eb96b5485789497a615b6c526600fa2b332789e5
ChristianLievrouw <celievrouw1@gmail.com>
2020-09-29 20:41:29 -0500

    added registering and login

--- Web/Pages/Account/Register.cshtml.cs
+++ Web/Pages/Account/Register.cshtml.cs
@@ -3,22 +3,56 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web.Models.Identity;
 
 namespace Web.Pages
 {
     public class RegisterModel : PageModel
     {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public RegisterModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager
+            )
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
         public void OnGet()
         {
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
             if (ModelState.IsValid)
             {
-                return LocalRedirect("~/");
+                var user = new ApplicationUser
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                };
+                var result = await userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+
+                    return LocalRedirect("~/");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    var errorKey =
+                        error.Code.Contains("Password") ? nameof(Input.Password) :
+                        error.Code.Contains("Email") ? nameof(Input.Email) :
+                        error.Code.Contains("UserName") ? nameof(Input.Email) :
+                        "";
+                    ModelState.AddModelError(nameof(Input) + "." + errorKey, error.Description);
+                }
             }
             return Page();
         }
