b49ece9807d9cfe1de2c8c9dda1f483510627f11
Keith Dahlby <keith@newbo.co>
2020-10-05 10:46:06 -0500

    Add Beer Style dropdown + some [Display(Name)]

--- Web/Models/Products/Beer.cs
+++ Web/Models/Products/Beer.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Web.Models.Products
 {
     public class Beer : Product
     {
+        [Display(Name = "Beer Style")]
         [EnumDataType(typeof(BeerStyle))]
         public BeerStyle BeerStyle { get; set; }
+
+        [Display(Name = "Alcohol by Volume")]
         public double Abv { get; set; }
+
+        [Display(Name = "IBU")]
         public int Ibu { get; set; }
     }
-
 
     public enum BeerStyle
     {
@@ -20,5 +23,4 @@ namespace Web.Models.Products
         Saison,
         Gose,
     }
-
 }
