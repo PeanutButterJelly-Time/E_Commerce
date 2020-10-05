3511fdf428bfa9028ff21bf0b77edd3ca2197bd2
Arik Mackenburg <Arikmackenburg@gmail.com>
2020-10-05 15:31:08 -0500

    Revert "Class Review"

--- Web/Models/Products/Beer.cs
+++ Web/Models/Products/Beer.cs
@@ -1,27 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.Models.Products
 {
     public class Beer : Product
     {
-        [Display(Name = "Beer Style")]
         [EnumDataType(typeof(BeerStyle))]
         public BeerStyle BeerStyle { get; set; }
-
-        [Display(Name = "Alcohol by Volume")]
         public double Abv { get; set; }
-
-        [Display(Name = "IBU")]
         public int Ibu { get; set; }
     }
 
+
     public enum BeerStyle
     {
-        // There is no 0
-        IPA = 1,
+        IPA,
         Sour,
         Stout,
         Saison,
         Gose,
     }
+
 }
