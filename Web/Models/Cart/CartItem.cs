fb586f816d81bb4fc93f3d7257522b2b2b312912
Keith Dahlby <keith@newbo.co>
2020-10-07 11:03:49 -0500

    Migration to fix CartItem FKs

--- Web/Models/Cart/CartItem.cs
+++ Web/Models/Cart/CartItem.cs
@@ -1,11 +1,18 @@
-﻿using Web.Models.Identity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Web.Models.Identity;
 
 namespace Web.Models.Cart
 {
     public class CartItem
     {
+        [ForeignKey(nameof(User))] // Optional in this case, but sometimes necessary
         public string UserId { get; set; }
-        public string ProductId { get; set; }
+
+        public int ProductId { get; set; }
+
+        // Navigation Properties
+        public ApplicationUser User { get; set; }
+
         public Product Product { get; set; }
     }
 }
